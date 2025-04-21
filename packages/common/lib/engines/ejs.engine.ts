e1f8dc2b60c514c0654442b07df67649817889ea
huang <58391800+sao-lang@users.noreply.github.com>
2025-04-21 10:05:45 +0800

    Update ejs.engine.ts

--- packages/common/lib/engines/ejs.engine.ts
+++ packages/common/lib/engines/ejs.engine.ts
@@ -9,34 +9,43 @@ import fs from 'fs/promises';
 
 export class EjsRenderer {
     format: (code: string, fileType: string) => string;
-    constructor (format: (code: string, fileType: string) => string) {
+    constructor(format: (code: string, fileType: string) => string) {
         this.format = format;
     }
-  async renderFromString(template: string, data: Record<string, any>, outputPath): Promise<string> {
-    const transformedCode = ejs.render(template, data, { async: true });
-      const ext = getFileExt<PrettierSupportFileType>(outputPath);
-      const formatedCode = this.format(transformedCode, ext) ?? transformedCode;
-      if (outputPath) {
-          
-          await mkDirs(dirname(outputPath));
-        await writeFile(outputPath, formatedCode, 'utf-8');
-      }
-  }
+    async renderFromString(
+        template: string,
+        data: Record<string, any>,
+        outputPath?: string,
+    ): Promise<string> {
+        const transformedCode = ejs.render(template, data, { async: true });
+        const ext = getFileExt<PrettierSupportFileType>(outputPath);
+        const formattedCode = this.format(transformedCode, ext) ?? transformedCode;
+        if (outputPath) {
+            await mkDirs(dirname(outputPath));
+            await writeFile(outputPath, formattedCode, 'utf-8');
+        }
+        return formattedCode;
+    }
 
-  /**
-   * 渲染模板文件
-   * @param filePath 模板文件路径（绝对路径或相对路径）
-   * @param data 注入的数据对象
-   */
-  async renderFromFile(filePath: string, data: Record<string, any>, outputPath: string): Promise<string> {
-    const absPath = path.resolve(filePath);
-    const template = await fs.readFile(absPath, 'utf-8');
-    const transformedCode this.renderFromString(template, data);
-      const ext = getFileExt<PrettierSupportFileType>(outputPath);
-      const formatedCode = this.format(transformedCode) ?? transformedCode;
-      if (outputPath) {
-          await mkDirs(dirname(outputPath));
-        await writeFile(outputPath, formatedCode, 'utf-8');
-      }
-  }
+    /**
+     * 渲染模板文件
+     * @param filePath 模板文件路径（绝对路径或相对路径）
+     * @param data 注入的数据对象
+     */
+    async renderFromFile(
+        filePath: string,
+        data: Record<string, any>,
+        outputPath?: string,
+    ): Promise<string> {
+        const absPath = path.resolve(filePath);
+        const template = await fs.readFile(absPath, 'utf-8');
+        const transformedCode = await this.renderFromString(template, data, outputPath);
+        const ext = getFileExt<PrettierSupportFileType>(outputPath);
+        const formattedCode = (await this.format(transformedCode, ext)) ?? transformedCode;
+        if (outputPath) {
+            await mkDirs(dirname(outputPath));
+            await writeFile(outputPath, formattedCode, 'utf-8');
+        }
+        return formattedCode;
+    }
 }
