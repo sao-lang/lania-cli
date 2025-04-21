3fb0a4faa484a89236b64f1b59baf07998e24486
huang <58391800+sao-lang@users.noreply.github.com>
2025-04-21 13:55:06 +0800

    Update cli-interaction.ts

--- packages/common/utils/cli-interaction.ts
+++ packages/common/utils/cli-interaction.ts
@@ -12,6 +12,10 @@ export interface Question<TCtx extends Context = Context>
     type: QuestionType;
     returnable?: boolean;
     timeout?: number;
+    choices?: {
+        name: string;
+        value: string;
+    }[];
     validate?: (
         input: any,
         answers: Answer,
