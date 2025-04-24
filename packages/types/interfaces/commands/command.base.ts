f446a405bf0a8a6850321348d2644e28f8283423
huang <58391800+sao-lang@users.noreply.github.com>
2025-04-24 11:23:10 +0800

    Update command.base.ts

--- packages/types/interfaces/commands/command.base.ts
+++ packages/types/interfaces/commands/command.base.ts
@@ -1,11 +1,22 @@
-export interface LaniaCommandActionInterface<Args extends any[] = any[]> {
-    handle(...args: Args): Promise<void>;
+export interface CommandOption {
+    flags: string;
+    description: string;
+    defaultValue?: any;
+    required?: boolean;
+    choices?: string[];
 }
 
 export interface CommandNeededArgsInterface {
     name: string;
     description?: string;
-    options: { flags: string; description?: string; defaultValue?: string | boolean | string[] }[];
+    options?: CommandOption[];
     alias?: string;
+    examples?: string[];
     helpDescription?: string;
+}
+
+export type CommandHook = () => Promise<void> | void;
+
+export interface LaniaCommandActionInterface<T extends any[] = any[]> {
+    handle: (...args: T) => Promise<void> | void;
 }
