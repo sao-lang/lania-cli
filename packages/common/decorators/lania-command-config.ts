12907026fe24d8a8f46d4ae67b0fbac0861ce2c1
huang <58391800+sao-lang@users.noreply.github.com>
2025-06-20 10:09:54 +0800

    Update lania-command-config.ts

--- packages/common/decorators/lania-command-config.ts
+++ packages/common/decorators/lania-command-config.ts
@@ -1,1 +1,20 @@
 
+import { CommandNeededArgsInterface } from '@lania-cli/types';
+import { LaniaCommand, LaniaCommandMetadata } from '../utils';
+
+// 定义元数据 key
+export const META_COMMAND_CONFIG = Symbol('lania:command_config');
+export function LaniaCommandConfig(
+    actor: new (...args: any[]) => any,
+    commandNeededArgs: CommandNeededArgsInterface,
+    subcommands: (new (...args: any[]) => LaniaCommand)[] = [],
+): ClassDecorator {
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    return function (target: Function) {
+        if (!actor || !commandNeededArgs?.name) {
+            throw new Error('@LaniaCommandConfig requires actor and commandNeededArgs.name');
+        }
+        const metadata: LaniaCommandMetadata = { actor, commandNeededArgs, subcommands };
+        Reflect.defineMetadata(META_COMMAND_CONFIG, metadata, target);
+    };
+}
